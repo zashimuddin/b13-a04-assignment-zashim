a8795416f2ea80e5c6a86c10b68422291e9204ff
zashimuddin <manhazashim@gmail.com>
2026-02-24 21:32:31 +0600

    render jop post successfull. It was difficult fix isuees

--- Script/script.js
+++ Script/script.js
@@ -11,6 +11,7 @@ const rejectedTabButton = document.getElementById("rejected-tab-btn");
 
 const allCardsSection = document.getElementById("allCards");
 const mainContainer = document.querySelector("main");
+const filterSection = document.getElementById("filtered-section");
 
 
 function counTotal(){
@@ -38,21 +39,241 @@ function toggleStyle(id){
 mainContainer.addEventListener("click", function(event){
     const parentNode = event.target.parentNode.parentNode;
     // console.log(event.target.parentNode.parentNode);
-
     
     const notAppliedBadge = parentNode.querySelector(".badge-not-applied").innerText;
     const interviewBadge = parentNode.querySelector(".badge-interview").innerText;
     const rejectedBadge = parentNode.querySelector(".badge-rejected").innerText;
+    const nameCompany = parentNode.querySelector(".company-name").innerText;
+    const positionJob = parentNode.querySelector(".position-job").innerText;
+    const typeJob = parentNode.querySelector(".type").innerText;
+    const salaryJob = parentNode.querySelector(".salary").innerText;
+    const descriptionJob = parentNode.querySelector(".description").innerText;
+    
     // console.log(notAppliedBadge, interviewBadge, rejectedBadge);
 
     const cardInfo = {
         notAppliedBadge, 
         interviewBadge, 
-        rejectedBadge
+        rejectedBadge,
+        nameCompany,
+        positionJob,
+        typeJob,
+        salaryJob,
+        descriptionJob
     }
-    console.log(cardInfo);
+
+    const jobPostExist = interviewList.find(item => item.nameCompany == cardInfo.nameCompany)
+
+    if(!jobPostExist){
+        interviewList.push(cardInfo);
+    }
+    renderJopPost();  
 
 });
+
+function renderJopPost(){
+    filterSection.innerHTML = " ";
+    
+    for(let interviews of interviewList){
+        console.log(interviews);
+        let div = document.createElement('div');
+        div.className = 'card card-border border-gray-300 bg-base-100 p-5  mx-auto';
+        div.innerHTML = `        <!-- job post 1 -->
+        <div class="card card-border border-gray-300 bg-base-100 p-5  mx-auto">
+            <!-- <div> -->
+                <div class="flex justify-between items-center">
+                    <h2 class="company-name text-2xl text-gray-600 font-bold">Mobile First Corp 1</h2>
+                    <button id="delete-img-btn">
+                        <img src="./assets/delete.png" alt="">
+                    </button>
+                </div>
+                <h3 class="position-job text-gray-500">React Native Developer </h3>
+                <h4 class="text-gray-500 py-4"><span class="location">Remote</span> . <span class="type">Full Time</span> . <span class="salary">$130,000 - $ 175,000</span></h4>
+                <div id="not-applied-badge"  class="badge-not-applied badge badge-soft badge-info text-black bg-gray-200 ">NOT APPLIED</div>
+                <div id="interview-badge" class="badge-interview badge badge-outline badge-success hidden">INTERVIEW</div>
+                <div id="rejected-badge" class="badge-rejected badge badge-outline badge-error hidden">REJECTED</div>
+                <p class="description">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <!-- </div> -->
+            <div class="pt-5 flex gap-3">
+                <button id="interview-btn" class="btn btn-outline btn-success">INTERVIEW</button>
+                <button id="rejected-btn" class="btn btn-outline btn-error">REJECTED</button> 
+            </div>
+
+        </div>
+        <!-- job post 1 ends -->
+
+        <!-- job post 2 -->
+        <div class="card card-border border-gray-300 bg-base-100 p-5  mx-auto">
+            <!-- <div> -->
+                <div class="flex justify-between items-center">
+                    <h2 class="company-name text-2xl text-gray-600 font-bold">Mobile First Corp 2</h2>
+                    <button id="delete-img-btn">
+                        <img src="./assets/delete.png" alt="">
+                    </button>
+                </div>
+                <h3 class="position-job text-gray-500">React Native Developer </h3>
+                <h4 class="text-gray-500 py-4"><span class="location">Remote</span> . <span class="type">Full Time</span> . <span class="salary">$130,000 - $ 175,000</span></h4>
+                <div id="not-applied-badge"  class="badge-not-applied badge badge-soft badge-info text-black bg-gray-200 ">NOT APPLIED</div>
+                <div id="interview-badge" class="badge-interview badge badge-outline badge-success hidden">INTERVIEW</div>
+                <div id="rejected-badge" class="badge-rejected badge badge-outline badge-error hidden">REJECTED</div>
+                <p class="description">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <!-- </div> -->
+            <div class="pt-5 flex gap-3">
+                <button id="interview-btn" class="btn btn-outline btn-success">INTERVIEW</button>
+                <button id="rejected-btn" class="btn btn-outline btn-error">REJECTED</button> 
+            </div>
+
+        </div>
+        <!-- job post 2 ends -->
+
+        <!-- job post 3 -->
+        <div class="card card-border border-gray-300 bg-base-100 p-5  mx-auto">
+            <!-- <div> -->
+                <div class="flex justify-between items-center">
+                    <h2 class="company-name text-2xl text-gray-600 font-bold">Mobile First Corp 3</h2>
+                    <button id="delete-img-btn">
+                        <img src="./assets/delete.png" alt="">
+                    </button>
+                </div>
+                <h3 class="position-job text-gray-500">React Native Developer </h3>
+                <h4 class="text-gray-500 py-4"><span class="location">Remote</span> . <span class="type">Full Time</span> . <span class="salary">$130,000 - $ 175,000</span></h4>
+                <div id="not-applied-badge"  class="badge-not-applied badge badge-soft badge-info text-black bg-gray-200 ">NOT APPLIED</div>
+                <div id="interview-badge" class="badge-interview badge badge-outline badge-success hidden">INTERVIEW</div>
+                <div id="rejected-badge" class="badge-rejected badge badge-outline badge-error hidden">REJECTED</div>
+                <p class="description">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <!-- </div> -->
+            <div class="pt-5 flex gap-3">
+                <button id="interview-btn" class="btn btn-outline btn-success">INTERVIEW</button>
+                <button id="rejected-btn" class="btn btn-outline btn-error">REJECTED</button> 
+            </div>
+
+        </div>
+        <!-- job post 3 ends -->
+
+        <!-- job post 4 -->
+         <div class="card card-border border-gray-300 bg-base-100 p-5  mx-auto">
+            <!-- <div> -->
+                <div class="flex justify-between items-center">
+                    <h2 class="company-name text-2xl text-gray-600 font-bold">Mobile First Corp 4</h2>
+                    <button id="delete-img-btn">
+                        <img src="./assets/delete.png" alt="">
+                    </button>
+                </div>
+                <h3 class="position-job text-gray-500">React Native Developer </h3>
+                <h4 class="text-gray-500 py-4"><span class="location">Remote</span> . <span class="type">Full Time</span> . <span class="salary">$130,000 - $ 175,000</span></h4>
+                <div id="not-applied-badge"  class="badge-not-applied badge badge-soft badge-info text-black bg-gray-200 ">NOT APPLIED</div>
+                <div id="interview-badge" class="badge-interview badge badge-outline badge-success hidden">INTERVIEW</div>
+                <div id="rejected-badge" class="badge-rejected badge badge-outline badge-error hidden">REJECTED</div>
+                <p class="description">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <!-- </div> -->
+            <div class="pt-5 flex gap-3">
+                <button id="interview-btn" class="btn btn-outline btn-success">INTERVIEW</button>
+                <button id="rejected-btn" class="btn btn-outline btn-error">REJECTED</button> 
+            </div>
+
+        </div>
+        <!-- job post 4 ends -->
+
+        <!-- job post 5 -->
+         <div class="card card-border border-gray-300 bg-base-100 p-5  mx-auto">
+            <!-- <div> -->
+                <div class="flex justify-between items-center">
+                    <h2 class="company-name text-2xl text-gray-600 font-bold">Mobile First Corp 5</h2>
+                    <button id="delete-img-btn">
+                        <img src="./assets/delete.png" alt="">
+                    </button>
+                </div>
+                <h3 class="position-job text-gray-500">React Native Developer </h3>
+                <h4 class="text-gray-500 py-4"><span class="location">Remote</span> . <span class="type">Full Time</span> . <span class="salary">$130,000 - $ 175,000</span></h4>
+                <div id="not-applied-badge"  class="badge-not-applied badge badge-soft badge-info text-black bg-gray-200 ">NOT APPLIED</div>
+                <div id="interview-badge" class="badge-interview badge badge-outline badge-success hidden">INTERVIEW</div>
+                <div id="rejected-badge" class="badge-rejected badge badge-outline badge-error hidden">REJECTED</div>
+                <p class="description">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <!-- </div> -->
+            <div class="pt-5 flex gap-3">
+                <button id="interview-btn" class="btn btn-outline btn-success">INTERVIEW</button>
+                <button id="rejected-btn" class="btn btn-outline btn-error">REJECTED</button> 
+            </div>
+
+        </div>
+        <!-- job post 5 ends -->
+
+        <!-- job post 6 -->
+         <div class="card card-border border-gray-300 bg-base-100 p-5  mx-auto">
+            <!-- <div> -->
+                <div class="flex justify-between items-center">
+                    <h2 class="company-name text-2xl text-gray-600 font-bold">Mobile First Corp 6</h2>
+                    <button id="delete-img-btn">
+                        <img src="./assets/delete.png" alt="">
+                    </button>
+                </div>
+                <h3 class="position-job text-gray-500">React Native Developer </h3>
+                <h4 class="text-gray-500 py-4"><span class="location">Remote</span> . <span class="type">Full Time</span> . <span class="salary">$130,000 - $ 175,000</span></h4>
+                <div id="not-applied-badge"  class="badge-not-applied badge badge-soft badge-info text-black bg-gray-200 ">NOT APPLIED</div>
+                <div id="interview-badge" class="badge-interview badge badge-outline badge-success hidden">INTERVIEW</div>
+                <div id="rejected-badge" class="badge-rejected badge badge-outline badge-error hidden">REJECTED</div>
+                <p class="description">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <!-- </div> -->
+            <div class="pt-5 flex gap-3">
+                <button id="interview-btn" class="btn btn-outline btn-success">INTERVIEW</button>
+                <button id="rejected-btn" class="btn btn-outline btn-error">REJECTED</button> 
+            </div>
+
+        </div>
+        <!-- job post 6 ends -->
+
+        <!-- job post 7 -->
+         <div class="card card-border border-gray-300 bg-base-100 p-5  mx-auto">
+            <!-- <div> -->
+                <div class="flex justify-between items-center">
+                    <h2 class="company-name text-2xl text-gray-600 font-bold">Mobile First Corp 7</h2>
+                    <button id="delete-img-btn">
+                        <img src="./assets/delete.png" alt="">
+                    </button>
+                </div>
+                <h3 class="position-job text-gray-500">React Native Developer </h3>
+                <h4 class="text-gray-500 py-4"><span class="location">Remote</span> . <span class="type">Full Time</span> . <span class="salary">$130,000 - $ 175,000</span></h4>
+                <div id="not-applied-badge"  class="badge-not-applied badge badge-soft badge-info text-black bg-gray-200 ">NOT APPLIED</div>
+                <div id="interview-badge" class="badge-interview badge badge-outline badge-success hidden">INTERVIEW</div>
+                <div id="rejected-badge" class="badge-rejected badge badge-outline badge-error hidden">REJECTED</div>
+                <p class="description">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <!-- </div> -->
+            <div class="pt-5 flex gap-3">
+                <button id="interview-btn" class="btn btn-outline btn-success">INTERVIEW</button>
+                <button id="rejected-btn" class="btn btn-outline btn-error">REJECTED</button> 
+            </div>
+
+        </div>
+        <!-- job post 7 ends -->
+
+        <!-- job post 8 -->
+         <div class="card card-border border-gray-300 bg-base-100 p-5  mx-auto">
+            <!-- <div> -->
+                <div class="flex justify-between items-center">
+                    <h2 class="company-name text-2xl text-gray-600 font-bold">Mobile First Corp 8</h2>
+                    <button id="delete-img-btn">
+                        <img src="./assets/delete.png" alt="">
+                    </button>
+                </div>
+                <h3 class="position-job text-gray-500">React Native Developer </h3>
+                <h4 class="text-gray-500 py-4"><span class="location">Remote</span> . <span class="type">Full Time</span> . <span class="salary">$130,000 - $ 175,000</span></h4>
+                <div id="not-applied-badge"  class="badge-not-applied badge badge-soft badge-info text-black bg-gray-200 ">NOT APPLIED</div>
+                <div id="interview-badge" class="badge-interview badge badge-outline badge-success hidden">INTERVIEW</div>
+                <div id="rejected-badge" class="badge-rejected badge badge-outline badge-error hidden">REJECTED</div>
+                <p class="description">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+            <!-- </div> -->
+            <div class="pt-5 flex gap-3">
+                <button id="interview-btn" class="btn btn-outline btn-success">INTERVIEW</button>
+                <button id="rejected-btn" class="btn btn-outline btn-error">REJECTED</button> 
+            </div>
+
+        </div>
+        <!-- job post 8 ends -->
+        
+        
+        `;
+    }
+}
 
 
 
